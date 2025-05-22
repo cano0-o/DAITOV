6ed39b0952367fe1bf8374ab23f755fc8d8ebd57
Emanuel Tavares <tavares2003@tutanota.com>
2025-05-22 16:47:17 -0700

    Modificación del esquema de base de datos para agregar el registro de los pacientes

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_14_165738) do
-  create_table "therapists", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "maternal_surname"
-    t.integer "age"
-    t.string "degree"
-    t.string "session_location"
-    t.string "password_digest", null: false
-    t.string "email", null: false
+ActiveRecord::Schema[8.0].define(version: 2025_05_22_220811) do
+  create_table "pacientes", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.bigint "tutor_id", null: false
+    t.string "nombre"
+    t.string "paternal_last_name"
+    t.string "maternal_last_name"
+    t.date "birth_date"
+    t.string "sexo"
+    t.string "nivel_discapacidad_cognitiva"
+    t.string "nivel_discapacidad_visual"
+    t.string "nivel_discapacidad_auditiva"
+    t.string "nivel_discapacidad_motriz_gruesa"
+    t.string "nivel_discapacidad_motriz_fina"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_therapists_on_email", unique: true
+    t.index ["tutor_id"], name: "index_pacientes_on_tutor_id"
   end
 
   create_table "tutors", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -43,4 +46,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_14_165738) do
     t.index ["email"], name: "index_tutors_on_email", unique: true
     t.index ["phone_number"], name: "index_tutors_on_phone_number", unique: true
   end
+
+  add_foreign_key "pacientes", "tutors"
 end
