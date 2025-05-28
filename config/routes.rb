e82fe0119f182148c3427f895685891e5e805966
Emanuel Tavares <tavares2003@tutanota.com>
2025-05-28 11:02:11 -0700

    Nuevas rutas para el paciente. En concreto para crear pacientes, indexarlos y mostrarlos.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
   get "tutors/create"
   get 'registro', to: 'tutors#new', as: :new_tutor
   post 'registro', to: 'tutors#create'
-  post '/pacientes', to: 'pacientes#create'
+
+  post '/pacientes', to: 'pacientes#create', as: :crear_paciente
+  get '/pacientes', to: 'pacientes#index', as: :pacientes
+  resources :pacientes, only: [:index, :create], path_names: {
+    index: 'buscar'
+  }
+  get 'pacientes/buscar', to: 'pacientes#buscar', as: :buscar_pacientes
+  resources :pacientes, only: [:show, :index, :create]
   
   get 'inicio-sesion', to: 'sessions#new', as: :new_session
   post 'inicio-sesion', to: 'sessions#create', as: :inicio_sesion
