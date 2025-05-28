9c19ba2efa2b237907deac115fead36cfc92f813
Emanuel Tavares <tavares2003@tutanota.com>
2025-05-28 11:06:43 -0700

    Los siguientes cambios son para requerir iniciar sesión antes de hacer cualquier cosa, implementar un índice con órden descendente por creación, se crea un método buscar para filtrar por id, nombre o apellidos, se agrega una vista show con manejo de errores para cuando los pacientes no han sido hallados y se centraliza lógica de autenticación con método require_login.

--- app/controllers/pacientes_controller.rb
+++ app/controllers/pacientes_controller.rb
@@ -1,4 +1,34 @@
 class PacientesController < ApplicationController
+  before_action :require_login
+
+  def index
+    @pacientes = current_tutor.pacientes.order(created_at: :desc)
+  end
+
+  def buscar
+    @pacientes = current_tutor.pacientes
+    if params[:busqueda].present?
+      search_term = "%#{params[:busqueda]}%"
+      @pacientes = @pacientes.where("nombre LIKE ? OR 
+                                   paternal_last_name LIKE ? OR 
+                                   maternal_last_name LIKE ? OR 
+                                   id = ?", 
+                                   search_term, search_term, search_term, 
+                                   params[:busqueda])
+    end
+    render :index
+  end
+  
+  def show
+    @paciente = current_tutor.pacientes.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to pacientes_path, alert: 'Paciente no encontrado'
+  end
+
+  def require_login
+    redirect_to new_session_path unless current_tutor
+  end
+  
   def create
     @paciente = current_tutor.pacientes.new(paciente_params)
     if @paciente.save
