45c92ba0a644a402dae01102529f680cadcfeeee
Emanuel Tavares <tavares2003@tutanota.com>
2025-05-28 11:10:29 -0700

    Implementación de helper para calcular la edad.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def age(birth_date)
+    return 'N/A' unless birth_date
+    
+    now = Time.now.utc.to_date
+    age = now.year - birth_date.year
+    age -= 1 if now.month < birth_date.month || (now.month == birth_date.month && now.day < birth_date.day)
+    age
+  end
 end
